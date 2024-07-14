967c3c3783277beea092f01ff221942f8129672d
Khalid Bin Walid <kbwalid26@gmail.com>
2024-07-14 18:55:49 +0900

    print location

--- location.h
+++ location.h
@@ -1,7 +1,13 @@
 #ifndef LOCATION_H
 #define LOCATION_H
+#include <sstream>
 struct Location {
   int line;
   int column;
 };
+
+inline std::ostream &operator<<(std::ostream &os, const Location &pos) {
+  os << pos.line << ":" << pos.column;
+  return os;
+}
 #endif
