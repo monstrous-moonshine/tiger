234bec49192cdc0282f5cb74f8b5504ffccb4c57
Khalid Bin Walid <kbwalid26@gmail.com>
2024-07-14 23:21:41 +0900

    run type-checking in main

--- main.cc
+++ main.cc
@@ -1,13 +1,18 @@
 #include "location.h"
 #include "print.h"
+#include "semant.h"
 #include "token.h"
 #include "tiger.tab.hh"
+#include "types.h"
+#include <cstring>
 // defined in lex.yy.cc
 int yylex_destroy();
 // defined in tiger.tab.cc
 extern uptr<absyn::ExprAST> parse_result;
 
 int main() {
+  symbol::Symbol sym_int(strdup("int"));
+  symbol::Symbol sym_str(strdup("string"));
   yy::parser parser;
   parser();
   // since we're done with the input, we don't need the lexer any more
@@ -15,7 +20,11 @@ int main() {
   if (parse_result) {
     absyn::print(0, *parse_result);
     std::printf("\n");
+    semant::Venv venv;
+    semant::Tenv tenv;
+    tenv.enter({sym_int, types::IntTy()});
+    tenv.enter({sym_str, types::StringTy()});
+    semant::trans_exp(venv, tenv, *parse_result);
   }
-  // since we've printed all the symbols, their strings can be freed
   symbol::Symbol::FreeAll();
 }
